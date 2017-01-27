952c42a9171d5f3b6eb973f6c9c57edbe0e74c91
codeNIH <codenih@gmail.com>
2017-01-26 21:01:43 -0500

    Update tickerScrape-1.py

--- Webscrape/Tutorial-Questions/tickerScrape-1.py
+++ Webscrape/Tutorial-Questions/tickerScrape-1.py
@@ -3,3 +3,5 @@
 # 2. Use provided TICKER to scrape appropriate Yahoo Finance page
 # 3. Use Beautiful Soup Package to retrieve current price for one share of stock, of specified company
 # 4. Print value to the console
+
+## Author: Alex (@alexg)
