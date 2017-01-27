f80e120632a16be56c188b8cb88f0a35036d38ca
codeNIH <codenih@gmail.com>
2017-01-26 21:01:05 -0500

    Create tickerScrape-2.py

---
+++ Webscrape/Tutorial-Questions/tickerScrape-2.py
@@ -0,0 +1,13 @@
+# Goal: To retrieve price of most recent trade performed by a security of interest using web scraping
+
+# Before looking at the code, try this on your own: 
+# Create a script which asks the user to input a ticker symbol, and returns the current stock price of that security
+
+# To complete this exercise, you will need to install/import both BeautifulSoup and requests (install using pip)
+
+#   Bonus:
+#       1) Print the date at which the stock price closed, and the full name of the security
+#       2) Create a script that asks the user if they would like to search another stock price
+
+
+## Author: Maxim (@moj)
