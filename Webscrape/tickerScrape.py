c7027b306ad5cf1fc6b120b65ca9c4e4524a3217
codeNIH <codenih@gmail.com>
2017-01-26 19:27:44 -0500

    Delete tickerScrape.py

--- Webscrape/tickerScrape.py
+++
@@ -1,5 +0,0 @@
-## Objective
-# 1. Prompt user to enter a TICKER symbol
-# 2. Use provided TICKER to scrape appropriate Yahoo Finance page
-# 3. Use Beautiful Soup Package to retrieve current price for one share of stock, of specified company
-# 4. Print value to the console
